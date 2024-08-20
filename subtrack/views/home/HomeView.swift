fbdeb366546ce67c2645cdb1c7ee4e4cec8519be
kametsun <kametsun7@gmail>
2024-08-20 22:05:40 +0900

    add: lint

--- subtrack/views/home/HomeView.swift
+++ subtrack/views/home/HomeView.swift
@@ -14,7 +14,7 @@ struct HomeView: View {
     init(viewModel: HomeViewModel) {
         self.viewModel = viewModel
     }
-
+    
     var body: some View {
         ZStack {
             Color.background
